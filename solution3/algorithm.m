13fdaa61d76995798ab28eb72ceebb59376524f4
SDU-GYF <a956895479@gmail.com>
2019-11-22 14:47:36 +0800

    solution3 matlab algorithm

---
+++ solution3/algorithm.m
@@ -0,0 +1,13 @@
+function [x]=algorithm(n_max)
+% 频率为1Hz，占空比为50%的周期方波信号
+t=-1:0.001:1;ft=square(0*pi*t,50);
+subplot(3,2,1),plot(t,ft),grid on
+axis([-1,1,-1.5,1.5])
+title('周期方波信号');
+str2double(n_max)
+n=1:2:n_max;
+b=4./(pi*n);
+x=b*sin(2*pi*n'*t);
+subplot(3,2,2),plot(t,x),grid on
+axis([-1,1,-1.5,1.5])
+title(['最大谐波次数=',num2str(n_max)])
